736b5e9bd775106d39eb548a6eab983f892ec82a
SilentSaint <47108605+SilentSaint@users.noreply.github.com>
2022-12-16 18:15:23 +0530

    server with request counts

--- servers/main.go
+++ servers/main.go
@@ -1,17 +1,32 @@
 package main
 
+// this server version is a minimal echo server
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
-// this server version is a minimal echo server
+// mutex(mutual exclusion) is used to avoid race conditions
+var mu sync.Mutex
+var count int
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.PATH=%q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
 }
