c61abcc398747507962df40dcd1056cfcb5f393c
SilentSaint <47108605+SilentSaint@users.noreply.github.com>
2022-12-19 22:38:50 +0530

    get all links from a website

---
+++ findLinks/main.go
@@ -0,0 +1,37 @@
+package main
+
+//this package aims to demonstrate a web crawler
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findLinks: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	//check node type and if anchor node then go through attr's and find href tag
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	//recursion to traverse the whole tree
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
