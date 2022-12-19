fcb0fd9fb7d5a502308d71a64d07936e72caa24f
SilentSaint <47108605+SilentSaint@users.noreply.github.com>
2022-12-19 21:49:26 +0530

    html/template demo

--- issues/issues.go
+++ issues/issues.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"golearning/github"
+	"html/template"
 	"log"
 	"os"
-	"text/template"
-	"time"
 )
 
-const templ = `{{.TotalCount}} issues:
-{{range .Items}}-----------------------------------
-Number: {{.Number}}
-User:   {{.User.Login}}
-Title:  {{.Title | printf "%.64s"}}
-Age:   	{{.CreatedAt | daysAgo}} days
+// const templ = `{{.TotalCount}} issues:
+// {{range .Items}}-----------------------------------
+// Number: {{.Number}}
+// User:   {{.User.Login}}
+// Title:  {{.Title | printf "%.64s"}}
+// Age:   	{{.CreatedAt | daysAgo}} days
+// {{end}}
+// `
+
+// func daysAgo(t time.Time) int {
+// 	return int(time.Since(t).Hours() / 24)
+// }
+
+// var report = template.Must(template.New("report").Funcs(template.FuncMap{"daysAgo": daysAgo}).Parse(templ))
+var issueList = template.Must(template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='textalign:left'>
+	<th>#</th>
+	<th>State</th>
+	<th>User</th>
+	<th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+	<td><a href='{{.HTMLURL}}'>{{.Number}}</td>
+	<td>{{.State}}</td>
+	<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+	<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
 {{end}}
-`
-
-func daysAgo(t time.Time) int {
-	return int(time.Since(t).Hours() / 24)
-}
-
-var report = template.Must(template.New("report").Funcs(template.FuncMap{"daysAgo": daysAgo}).Parse(templ))
+</table>
+`))
 
 func main() {
 
@@ -29,7 +47,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := report.Execute(os.Stdout, result); err != nil {
+	if err := issueList.Execute(os.Stdout, result); err != nil {
 		log.Fatal(err)
 	}
 }
