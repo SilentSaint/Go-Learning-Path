04e046c7357409b1d80d49a3eafb2080e0a172f5
SilentSaint <47108605+SilentSaint@users.noreply.github.com>
2022-12-20 17:48:55 +0530

    get title demo defered functions

--- findLinks/main.go
+++ findLinks/main.go
@@ -5,45 +5,71 @@ package main
 import (
 	"fmt"
 	"golearning/links"
-	"log"
+	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
-func breadthFirst(f func(item string) []string, worklist []string) {
-	seen := make(map[string]bool)
-	for len(worklist) > 0 {
-		items := worklist
-		worklist = nil
-		for _, item := range items {
-			if !seen[item] {
-				seen[item] = true
-				worklist = append(worklist, f(item)...)
-			}
+// func breadthFirst(f func(item string) []string, worklist []string) {
+// 	seen := make(map[string]bool)
+// 	for len(worklist) > 0 {
+// 		items := worklist
+// 		worklist = nil
+// 		for _, item := range items {
+// 			if !seen[item] {
+// 				seen[item] = true
+// 				worklist = append(worklist, f(item)...)
+// 			}
+// 		}
+// 	}
+// }
+// func crawl(url string) []string {
+// 	fmt.Println(url)
+// 	list, err := links.Extract(url)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// 	return list
+// }
+
+func title(url string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	ct := res.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		return fmt.Errorf("parsing %s as html: %v", url, err)
+	}
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
 		}
 	}
-}
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := links.Extract(url)
-	if err != nil {
-		log.Print(err)
-	}
-	return list
+	links.ForEachNode(doc, visitNode, nil)
+	return nil
 }
 func main() {
-	// for _, url := range os.Args[1:] {
-	// 	links, err := findLinks(url)
-	// 	if err != nil {
-	// 		fmt.Fprintf(os.Stderr, "findLinks: %v\n", err)
-	// 		continue
-	// 	}
+	for _, url := range os.Args[1:] {
+		if err := title(url); err != nil {
+			fmt.Fprintf(os.Stderr, "Title: %v\n", err)
+		}
+	}
 	// 	for _, link := range links {
 	// 		fmt.Print(link)
 	// 	}
 	// }
 	//crawl the web first web first
 	//starting from the command line
-	breadthFirst(crawl, os.Args[1:])
+	// breadthFirst(crawl, os.Args[1:])
 }
 
 // func visit(links []string, n *html.Node) []string {
